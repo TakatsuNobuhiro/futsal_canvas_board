c4a0e32e60eadb12bfab20e1c3ae0dd21dcdc26f
高津亘宏 <nobuhiro19980622@gmail.com>
2022-06-16 01:24:04 +0900

    feat firebase 環境設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,14 +36,17 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    '@nuxt/typescript-build'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    'bootstrap-vue/nuxt'
   ],
+  generate: {
+    dir: 'public'
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
