0fa99393d2bbcb6167403a3dbbb2b4413b831103
高津亘宏 <nobuhiro19980622@gmail.com>
2022-06-16 22:05:07 +0900

    feat meta情報

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,18 +7,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'futsal_canvas_board',
+    title: 'フットサル戦術ボード',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'フットテック式web版フットサル戦術ボード' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/image/board.png' }
     ]
   },
 
