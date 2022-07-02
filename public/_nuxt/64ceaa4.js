94491a70a884c59ab08c6da899feb40c73e00db8
高津亘宏 <nobuhiro19980622@gmail.com>
2022-07-02 14:48:34 +0900

    feat npm run generate

--- public/_nuxt/64ceaa4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{225:function(n,o,w){w(226),n.exports=w(227)}},[[225,5,2,6]]]);
--- public/_nuxt/794bf1b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{303:function(e,n,t){"use strict";t.r(n);var l=t(8).default.extend({}),o=t(77),component=Object(o.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("Board")}),[],!1,null,null,null);n.default=component.exports;installComponents(component,{Board:t(299).default})}}]);
